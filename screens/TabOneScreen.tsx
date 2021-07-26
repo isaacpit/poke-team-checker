984995ed4f8febbe0bd128c8db438747d70f343b
isaacpitblado <isaacpitblado@tamu.edu>
2021-07-25 22:33:23 -0500

    merge conflict

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet } from 'react-native';
 import { HStack, Stack, Center, NativeBaseProvider } from "native-base";
+
+// import { useState } from 'react';
+
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+const pokeData = require('../data/pokemon-info-small.json');
+
 export default function TabOneScreen() {
+  const [data, setData] = useState(pokeData);
+  console.log("data here: ", data);
+  const typeList = data["type-list"];
+  console.log("type-list: ", typeList);
+
   return (
     <View style={styles.container}>
     <Stack space={3} alignItems="center">
@@ -48,6 +58,34 @@ export default function TabOneScreen() {
 
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {/* <Text> */}
+      {/* {typeList.map(type => <Text key={type.toString()}> {type["resistant-to"].map(weakness => {weakness},)}</Text>)} */}
+      <ListTypes typeList={typeList}/>
+      {/* </Text> */}
+
+    </View>
+  );
+}
+
+function ListTypes ({typeList}) {
+  console.log("ListTypes: ", typeList);
+  const typeListLength = typeList.length;
+  console.log("typeList LEngth: ", typeListLength);
+  // typeList.map((type, i) => console.log(i, type));
+  const components = typeList.map((type, i) => (
+    // console.log("typer: ", type.type))
+    console.log(type),
+    <PokeType key={i} pokeName={type.type}></PokeType>));
+  console.log(components);
+  return (<View>{components}</View>);
+}
+
+function PokeType ({ pokeName }) {
+  console.log("props: ");
+  console.log("name: ", pokeName);
+  return (
+    <View>
+      <Text>Type {pokeName}</Text>
     </View>
   );
 }
