e846da8b2ac16348101834fb7792fc8c7e402f59
WetDuk <tony.qing@tamu.edu>
2021-07-27 14:29:24 -0500

    Added type svgs and pokemon icons

---
+++ components/PokeList.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { List, Container, Box, ScrollView, Image } from "native-base";
+import { pokeDataLarge } from "../screens/TabOneScreen";
+import Bug from "../assets/icons/bug.svg";
+import Dark from "../assets/icons/dark.svg";
+import Dragon from "../assets/icons/dragon.svg";
+import Electric from "../assets/icons/electric.svg";
+import Fairy from "../assets/icons/fairy.svg";
+import Fighting from "../assets/icons/fighting.svg";
+import Fire from "../assets/icons/fire.svg";
+import Flying from "../assets/icons/flying.svg";
+import Ghost from "../assets/icons/ghost.svg";
+import Grass from "../assets/icons/grass.svg";
+import Ground from "../assets/icons/ground.svg";
+import Ice from "../assets/icons/ice.svg";
+import Normal from "../assets/icons/normal.svg";
+import Poison from "../assets/icons/poison.svg";
+import Psychic from "../assets/icons/psychic.svg";
+import Rock from "../assets/icons/rock.svg";
+import Steel from "../assets/icons/steel.svg";
+
+export default function PokeList() {
+  console.log(pokeDataLarge);
+
+  return (
+    <View
+      style={{
+        width: "80%",
+        height: "65%",
+      }}
+    >
+      <ScrollView>
+        <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
+          <Bug height={11} width={11} />
+          <Dark height={11} width={11} />
+          <Dragon height={11} width={11} />
+          <Electric height={11} width={11} />
+          <Fairy height={11} width={11} />
+          <Fighting height={11} width={11} />
+          <Fire height={11} width={11} />
+          <Flying height={11} width={11} />
+          <Ghost height={11} width={11} />
+          <Grass height={11} width={11} />
+          <Ground height={11} width={11} />
+          <Ice height={11} width={11} />
+          <Normal height={11} width={11} />
+          <Poison height={11} width={11} />
+          <Psychic height={11} width={11} />
+          <Rock height={11} width={11} />
+          <Steel height={11} width={11} />
+        </View>
+        <List
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {pokeDataLarge.map(({ name, sprites }, i) => {
+            return (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  margin: 10,
+                }}
+              >
+                <Box bg="">
+                  <Image
+                    size={"sm"}
+                    alt={`imagine ${name}`}
+                    source={{ uri: sprites }}
+                  ></Image>
+                </Box>
+                <Text style={styles.text}>{name}</Text>
+              </View>
+            );
+          })}
+        </List>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    flexWrap: "nowrap",
+  },
+});
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import { HStack, Stack, Center, NativeBaseProvider } from "native-base";
-
+import PokeList from "../components/PokeList";
 // import { useState } from 'react';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+export const pokeDataLarge = require("../data/poke-data-large.json");
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
-const pokeData = require('../data/pokemon-info-small.json');
+const pokeData = require("../data/pokemon-info-small.json");
 
 export default function TabOneScreen() {
   const [data, setData] = useState(pokeData);
@@ -17,70 +17,75 @@ export default function TabOneScreen() {
 
   return (
     <View style={styles.container}>
-    <Stack space={3} alignItems="center">
-      {/* <Heading>HStack</Heading> */}
-      <HStack space={3} alignItems="center">
-        <Center
-          size={24}
-          bg="primary.400"
-          rounded="md"
-          _text={{
-            color: "white",
-          }}
-          shadow={3}
-        >
-          Box 1
-        </Center>
-        <Center
-          bg="secondary.400"
-          size={24}
-          rounded="md"
-          _text={{
-            color: "white",
-          }}
-          shadow={3}
-        >
-          Box 2
-        </Center>
-        <Center
-          size={24}
-          bg="emerald.400"
-          rounded="md"
-          _text={{
-            color: "white",
-          }}
-          shadow={3}
-        >
-          Box 3
-        </Center>
-      </HStack>
-    </Stack>
-
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Stack space={3} alignItems="center">
+        {/* <Heading>HStack</Heading> */}
+        <HStack space={3} alignItems="center">
+          <Center
+            size={24}
+            bg="primary.400"
+            rounded="md"
+            _text={{
+              color: "white",
+            }}
+            shadow={3}
+          >
+            Box 1
+          </Center>
+          <Center
+            bg="secondary.400"
+            size={24}
+            rounded="md"
+            _text={{
+              color: "white",
+            }}
+            shadow={3}
+          >
+            Box 2
+          </Center>
+          <Center
+            size={24}
+            bg="emerald.400"
+            rounded="md"
+            _text={{
+              color: "white",
+            }}
+            shadow={3}
+          >
+            Box 3
+          </Center>
+        </HStack>
+      </Stack>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
       {/* <Text> */}
       {/* {typeList.map(type => <Text key={type.toString()}> {type["resistant-to"].map(weakness => {weakness},)}</Text>)} */}
-      <ListTypes typeList={typeList}/>
+      {/* <ListTypes typeList={typeList} /> */}
+      <PokeList />
       {/* </Text> */}
-
     </View>
   );
 }
 
-function ListTypes ({typeList}) {
+function ListTypes({ typeList }) {
   console.log("ListTypes: ", typeList);
   const typeListLength = typeList.length;
   console.log("typeList LEngth: ", typeListLength);
   // typeList.map((type, i) => console.log(i, type));
-  const components = typeList.map((type, i) => (
-    // console.log("typer: ", type.type))
-    console.log(type),
-    <PokeType key={i} pokeName={type.type}></PokeType>));
+  const components = typeList.map(
+    (type, i) => (
+      // console.log("typer: ", type.type))
+      console.log(type), (<PokeType key={i} pokeName={type.type}></PokeType>)
+    )
+  );
   console.log(components);
-  return (<View>{components}</View>);
+  return <View>{components}</View>;
 }
 
-function PokeType ({ pokeName }) {
+function PokeType({ pokeName }) {
   console.log("props: ");
   console.log("name: ", pokeName);
   return (
@@ -93,16 +98,16 @@ function PokeType ({ pokeName }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
